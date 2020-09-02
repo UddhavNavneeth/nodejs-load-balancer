d0a5ab086ebee97903c9fcb69fc54e6e53d9b020
unknown <uddhav.navneeth@gmail.com>
2020-09-02 14:40:43 +0530

    scrappy basic load balancer, pm2 useless

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.argv[2] || process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send(`Hello from port ${port}`);
+    console.log(`Hello from port ${port}`);
 });
 
 app.listen(port, () => {
